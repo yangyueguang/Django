d2e06aebbdd85f02080a7f17cf92456886b1b703
yangyueguang <2829969299@qq.com>
2020-02-15 18:54:17 +0800

    update

--- apps/models/models.py
+++ apps/models/models.py
@@ -2,10 +2,25 @@ from django.db import models
 import datetime
 
 
+# 自定义查询对象
+class StudentManager(models.manager):
+    def get_queryset(self):
+        return super(StudentManager, self).get_queryset().filter(isDelete=False)
+
+
 class Student(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
-    #borthday = models.DateTimeField(default=datetime.date(year=1990,month=10,day=12))
+    isDelete = models.BooleanField(default=False)
+    # borthday = models.DateTimeField(default=datetime.date(year=1990,month=10,day=12))
+
+    # 自定义表名
+    class Meta:
+        db_table = 'custom_table'
+
+    students1 = models.Manager()
+    students2 = StudentManager()
+
 
 class Teacher(models.Model):
     name = models.CharField(max_length=20)
